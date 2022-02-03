94d81b7172fdd0f36e64f0778e7a9315a2bc4674
Carlos E <elizaldc@protonmail.com>
2022-02-03 10:00:19 -0600

    Added password validation to form using JS.

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 const password = document.getElementById('password');
+const confirmPassWord = document.getElementById('confirm');
+const myForm = document.getElementById('myForm');
+myForm.noValidate = true;
 
+myForm.addEventListener('submit', validateForm)
+
+function validateForm(e) {
+    const form = e.target;
+
+    if (form.checkValidity()) {
+        //Form is valid - make further checks
+        if (password.value !== confirmPassWord.value) {
+            e.preventDefault();
+            password.classList.add('error');
+            confirmPassWord.classList.add('error');
+            const errorText = document.getElementById('password-text');
+            errorText.classList.add('error-text');
+            errorText.innerText = "*Passwords do not match"
+        }
+    } 
+
+    else {
+        //Form is invalid - cancel submit
+        e.preventDefault();
+    }
+}
