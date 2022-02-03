3748e686c32027531f3c52f353553e7f2ae3f3c3
Carlos E <elizaldc@protonmail.com>
2022-02-03 12:42:36 -0600

    Added validation for form fields through styling.

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 const password = document.getElementById('password');
 const confirmPassWord = document.getElementById('confirm');
 const myForm = document.getElementById('myForm');
-myForm.noValidate = true;
 
-myForm.addEventListener('submit', validateForm)
+
+myForm.addEventListener('submit', validateForm);
 
 function validateForm(e) {
     const form = e.target;
-
+    console.log(form.checkValidity());
     if (form.checkValidity()) {
         //Form is valid - make further checks
+        
+    } 
+
+    else {
+        //Form is invalid - cancel submit
+        e.preventDefault();
         if (password.value !== confirmPassWord.value) {
             e.preventDefault();
             password.classList.add('error');
             confirmPassWord.classList.add('error');
             const errorText = document.getElementById('password-text');
             errorText.classList.add('error-text');
-            errorText.innerText = "*Passwords do not match"
+            errorText.innerText = "*Passwords do not match";
         }
-    } 
-
-    else {
-        //Form is invalid - cancel submit
-        e.preventDefault();
+        Array.from(form.elements).forEach(i => {
+            if (i.checkValidity()) {
+                i.classList.remove('error');
+                //document.getElementsByClassName('help').style.display = "none";
+            }
+            else {
+                i.classList.add('error');
+                //document.getElementsByClassName('help').style.display = "block";
+            }
+        })
     }
-}
+};
